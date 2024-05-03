496bf2491de6fd00ec86f21151410a9760433046
Joska <joska.gyur@gmail.com>
2024-05-03 13:32:23 +0200

    add full width to product card text container

--- src/components/shared/CardProduct.tsx
+++ src/components/shared/CardProduct.tsx
@@ -13,7 +13,7 @@ const CardProduct = ({ title, subtitle, image, altText }: CardProductProps) => {
       <div className="md:max-w-[50%] sm:max-w-full">
         <img className="h-max w-max object-fill" src={image} alt={altText} />
       </div>
-      <div className="group flex flex-col justify-center bg-[#FDF3F0] p-8 text-center duration-100 hover:bg-peach h-full">
+      <div className="group flex flex-col justify-center bg-[#FDF3F0] p-8 text-center duration-100 hover:bg-peach w-full h-full">
         <h3 className="mb-4 uppercase text-peach group-hover:text-white">
           {title}
         </h3>
