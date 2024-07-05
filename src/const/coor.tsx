ebaae8ce11948a6c8c11fbdf084a905cff2aebcf
Joska <joska.gyur@gmail.com>
2024-07-05 13:52:29 +0200

    small change Canada office latitude Leaflet

--- src/const/coor.tsx
+++ src/const/coor.tsx
@@ -1,4 +1,4 @@
-const latCanadaOffice = 43.70664501702474;
+const latCanadaOffice = 43.71674501702580;
 const longCanadaOffice = -79.25059661246068;
 const latAustraliaOffice = -33.097320141327586;
 const longAustraliaOffice = 151.6158313530451;
